361d31067f1dd9f4a240823ddfcec75184b036bf
griffio <griffio@users.noreply.github.com>
2016-10-01 13:16:47 +0100

    Add test

---
+++ src/test/kotlin/griffio/SolarSystemTests.kt
@@ -0,0 +1,20 @@
+package griffio
+
+import griffio.components.DaggerSolarSystem
+import griffio.modules.OuterPlanetsModule
+import griffio.modules.TerrestrialPlanetsModule
+import org.junit.Test
+
+class SolarSystemTests {
+
+  @Test
+  fun `earth is 1 au`() {
+
+    val solarSystem = DaggerSolarSystem.builder()
+        .terrestrialPlanetsModule(TerrestrialPlanetsModule())
+        .outerPlanetsModule(OuterPlanetsModule())
+        .build()
+
+    assert(solarSystem.terrestrial().earth.au() == 1.0)
+  }
+}
