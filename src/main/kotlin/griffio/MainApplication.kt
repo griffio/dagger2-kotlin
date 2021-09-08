c68d4da7cf61be314494599ba7d57e3b9cf333bb
griffio <griffio@users.noreply.github.com>
2021-09-08 16:07:58 +0100

    Change to bigdecimal
    kotlin querydsl serializer issue with Double to double

--- src/main/kotlin/griffio/MainApplication.kt
+++ src/main/kotlin/griffio/MainApplication.kt
@@ -8,36 +8,38 @@ import griffio.entity.Satellite
 import griffio.modules.OuterPlanetsModule
 import griffio.modules.TerrestrialPlanetsModule
 import griffio.planets.SomePlanetsEnum
+import java.math.BigDecimal
 
 class MainApplication {
 
-  fun solarSystem(): SolarSystem {
-    return DaggerSolarSystem.builder()
-        .terrestrialPlanetsModule(TerrestrialPlanetsModule())
-        .outerPlanetsModule(OuterPlanetsModule())
-        .build()
-  }
-
-  fun findSatellite(satellites: List<Satellite>): Satellite {
-    return CollQueryFactory
-        .from(QSatellite.satellite, satellites)
-        .where(QSatellite.satellite.diameter.between(3000.0, 4000.0))
-        .fetchFirst()
-  }
-
-  companion object {
-
-    @JvmStatic fun main(args: Array<String>) {
-
-      val main = MainApplication()
-      val solarSystem = main.solarSystem()
-      println(solarSystem.terrestrial())
-      println(solarSystem.outer())
-      println(solarSystem.planets().map[SomePlanetsEnum.EARTH])
-
-      val someMoons = arrayListOf(Satellite(1L, "Callisto", 4800.0), Satellite(2L, "Luna", 3476.0))
-      val moon = main.findSatellite(someMoons)
-      println(moon)
+    fun solarSystem(): SolarSystem {
+        return DaggerSolarSystem.builder()
+            .terrestrialPlanetsModule(TerrestrialPlanetsModule())
+            .outerPlanetsModule(OuterPlanetsModule())
+            .build()
     }
-  }
+
+    fun findSatellite(satellites: List<Satellite>): Satellite {
+        return CollQueryFactory
+            .from(QSatellite.satellite, satellites)
+            .where(QSatellite.satellite.diameter.between(3000.0, 4000.0))
+            .fetchFirst()
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+
+            val main = MainApplication()
+            val solarSystem = main.solarSystem()
+            println(solarSystem.terrestrial())
+            println(solarSystem.outer())
+            println(solarSystem.planets().map[SomePlanetsEnum.EARTH])
+
+            val someMoons = arrayListOf(Satellite(1L, "Callisto", BigDecimal("4800.0")), Satellite(2L, "Luna",BigDecimal("3476.0")))
+            val moon = main.findSatellite(someMoons)
+            println(moon)
+        }
+    }
 }
--- src/main/kotlin/griffio/entity/Satellite.kt
+++ src/main/kotlin/griffio/entity/Satellite.kt
@@ -1,8 +1,9 @@
 package griffio.entity
 
+import java.math.BigDecimal
 import javax.persistence.Entity
 
-@Entity class Satellite(var id : Long, var name : String, var diameter : Double) {
+@Entity class Satellite(var id: Long, var name: String, var diameter: BigDecimal) {
 
     override fun toString(): String {
         return "Satellite:$id:$name:$diameter (km)"
